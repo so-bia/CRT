369d0783dff8f801ac0d16f2a5b8c573935a3653
Aashritha <164225758+Aashritha014@users.noreply.github.com>
2025-09-18 14:59:56 +0530

    Update 844.Backspace string compare.java

--- 844.Backspace string compare.java
+++ 844.Backspace string compare.java
@@ -27,3 +27,46 @@ class Solution {
         
     }
 }
+
+/*  SECONF APPROCAH 
+
+class Solution 
+{
+    public boolean backspaceCompare(String s, String t) 
+    {
+         Stack<Character> s1 = new Stack<>();
+        for(char ch : s.toCharArray())
+        {
+            if(ch=='#')
+            {
+                if(!s1.isEmpty())
+                {
+                    s1.pop();
+                }
+            }
+            else 
+            {
+                s1.push(ch);
+            }
+        }
+        Stack<Character> s2 = new Stack<>();
+        for(char ch : t.toCharArray())
+        {
+            if(ch=='#')
+            {
+                if(!s2.isEmpty())
+                {
+                    s2.pop();
+                }
+            }
+            else 
+            {
+                s2.push(ch);
+            }
+        }
+        return s1.equals(s2);
+    }
+}
+
+
+*/
