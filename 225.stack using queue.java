4737a4bdb28c35a49462c4bf565ce4bdc9ef1361
so-bia <160761527+so-bia@users.noreply.github.com>
2025-10-09 15:11:23 +0530

    Create 225.stack using queue.java

---
+++ 225.stack using queue.java
@@ -0,0 +1,27 @@
+class MyStack {
+    Queue <Integer> q1;
+    public MyStack() {
+        q1=new LinkedList();
+    }
+    
+    public void push(int x) {
+        q1.add(x);
+        for(int i=0;i<q1.size()-1;i++)
+        q1.add(q1.remove());
+    }
+    
+    public int pop() {
+      
+          return q1.remove();
+
+    }
+    
+    public int top() {
+        return q1.peek();
+
+    }
+    
+    public boolean empty() {
+        return q1.isEmpty();
+    }
+}
